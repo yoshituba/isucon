82990ee11a87962dff032b033373b1a5db113207
Yoshi <aaa465abc.jj.wf@gmail.com>
2018-09-15 14:51:30 +0900

    tmp

--- webapp/go/src/isubata/app.go
+++ webapp/go/src/isubata/app.go
@@ -736,7 +736,7 @@ func main() {
 	e.Use(middleware.Static("../public"))
 
 	e.GET("/initialize", getInitialize)
-	e.GET("/", getIndex)
+	e.GET("/", getIcon)
 	e.GET("/register", getRegister)
 	e.POST("/register", postRegister)
 	e.GET("/login", getLogin)
